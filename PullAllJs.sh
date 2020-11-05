b09e72ac18c56d5e9cfb44604b9403addb244454
Othmane Tamagart <SQLi@outlook.fr>
2020-11-05 01:32:54 +0100

    HackCheckURL + Better output
    - The integration of HackCheckURL.
    - Increasing the maturity of PullAllJS' output.

--- PullAllJs.sh
+++ PullAllJs.sh
@@ -4,4 +4,14 @@ then
         exit 1
 fi
 
-waybackurls $1 | grep "\.js" | uniq | sort >> PulledJS-$1.txt
+# Using WayBackURLs to grab JS files and save them to output/ directory.
+echo "Looking for .js files..."
+waybackurls $1 | grep "\.js" | uniq | sort >> /tmp/$1-waybackurls.txt
+
+# Using hakcheckurl to test the responses of the links we grabbed
+echo "Testing URLs..."
+cat /tmp/$1-waybackurls.txt | hakcheckurl >> output/$1-PulledJS.txt
+
+
+echo "[+] Done."
+echo "Please find your target's JS files at the output/ directory"
