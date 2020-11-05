50306db441feffe66a61164dd5a6487a8342ee39
Othmane Tamagart <SQLi@outlook.fr>
2020-11-05 01:36:18 +0100

    Added HackCheckURL + copying of binaries

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+echo "Installing..."
+# Downloading tools using go.
 go get github.com/tomnomnom/waybackurls
+go get github.com/hakluke/hakcheckurl
+
+# Copying the binaries to the dir `/usr/bin/`
+sudo cp ~/go/bin/waybackurls /usr/bin/
+sudo cp ~/go/bin/hackcheckurl /usr/bin/
+echo "Done."
